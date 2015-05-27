68ecf840c799dd0a59c24a8c76a268deba1af964
John Bowden <j.j.b1980@live.com>
2015-05-28 07:57:15 +1000

    more stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var webserver = require('gulp-webserver');
 
 gulp.task('less1', function() {
     return gulp.src('css/styles.less')
@@ -8,9 +9,19 @@ gulp.task('less1', function() {
         .pipe(gulp.dest('css'));
 });
 
+gulp.task('webserver', function() {
+    gulp.src('')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            open: true,
+            port: 3100,
+            fallback: 'index.html'
+        }));
+});
 
 gulp.task('watch', function() {
     gulp.watch('css/styles.less', ['less1']);  // Watch the .less file, then run the less task
 });
 
-gulp.task('default', ['less1','watch']); // Default will run the 'less' and 'watch' task
+gulp.task('default', ['less1','watch', 'webserver']); // Default will run the 'less' and 'watch' task
